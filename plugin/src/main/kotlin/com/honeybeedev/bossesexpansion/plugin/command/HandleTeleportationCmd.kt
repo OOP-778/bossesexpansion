ec2dbb1546ff67692969d7cdb0210f7d3ed26506
oop-778 <oskardhavel@gmail.com>
2021-03-25 21:01:50 +0200

    Added times teleported check for HandleTeleportationCmd

--- plugin/src/main/kotlin/com/honeybeedev/bossesexpansion/plugin/command/HandleTeleportationCmd.kt
+++ plugin/src/main/kotlin/com/honeybeedev/bossesexpansion/plugin/command/HandleTeleportationCmd.kt
@@ -1,7 +1,9 @@
 package com.honeybeedev.bossesexpansion.plugin.command
 
 import com.honeybeedev.bossesexpansion.plugin.BossesExpansion
+import com.honeybeedev.bossesexpansion.plugin.boss.BBoss
 import com.honeybeedev.bossesexpansion.plugin.command.argument.UUIDArg
+import com.honeybeedev.bossesexpansion.plugin.config.group.action.SpawnAction
 import com.oop.orangeengine.command.OCommand
 import com.oop.orangeengine.main.task.StaticTask
 import java.util.*
@@ -13,7 +15,20 @@ class HandleTeleportationCmd : OCommand() {
         argument(UUIDArg().setRequired(true))
         onCommand {
             val uuid = it.getArgAsReq<UUID>("uuid")
+
             BossesExpansion.instance!!.bossController.getBoss(uuid)?.let { boss ->
+                val times = (boss as BBoss).playersTeleported.merge(it.senderAsPlayer.uniqueId, 1) { v1, v2 ->
+                    v1 + v2
+                }!!
+
+                if (!(it.senderAsPlayer.isOp || it.senderAsPlayer.hasPermission("be.bypassTeleportLimits"))) {
+                    boss.bossGroup?.action<SpawnAction>()?.spawnMessage?.teleportLimit?.let {
+                        if (it < 0) return@let
+
+                        if (it < times) return@onCommand
+                    }
+                }
+
                 StaticTask.getInstance().ensureSync { it.senderAsPlayer.teleport(boss.location) }
             }
         }
